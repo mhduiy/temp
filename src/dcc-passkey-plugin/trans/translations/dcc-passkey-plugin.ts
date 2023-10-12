0d121df5a5b9976c92f7d08f8c83dfd66bd29b51
chujiaqi <chujiaqi@uniontech.com>
2023-10-12 15:36:16 +0800

    fix: dcc-passkey-plugin翻译文件结构整理
    dcc-passkey-plugin翻译文件结构整理
    
    Log: dcc-passkey-plugin翻译文件结构整理
    Task: https://pms.uniontech.com/task-view-286601.html
    Influence: 项目内部文件结构整理
    Change-Id: I4c1bcb5fbc22fdb623eca7276068ab317d285d5e

---
+++ src/dcc-passkey-plugin/trans/translations/dcc-passkey-plugin.ts
@@ -0,0 +1,287 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>KeyPinDialogCtrl</name>
+    <message>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="58"/>
+        <source>设置安全密钥PIN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="60"/>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="161"/>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="279"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="61"/>
+        <source>Set</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="77"/>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="190"/>
+        <source>新的PIN：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="81"/>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="93"/>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="182"/>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="194"/>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="206"/>
+        <source>必填</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="89"/>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="202"/>
+        <source>重复PIN：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="159"/>
+        <source>更改安全密钥PIN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="162"/>
+        <source>Change</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="178"/>
+        <source>旧的PIN：</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="298"/>
+        <source>操作失败！</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="326"/>
+        <source>PIN 不能为空</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="329"/>
+        <source>PIN 长度不能少于4个字符</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="332"/>
+        <source>PIN 长度不能超过63个字符</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="348"/>
+        <source>PIN 不一致</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ManageWidget</name>
+    <message>
+        <location filename="../../window/widgets/managewidget.cpp" line="29"/>
+        <source>Passkey Manage</source>
+        <translation type="unfinished"></translation>
+        <extra-contents_path>/passkey/Passkey Manage</extra-contents_path>
+    </message>
+    <message>
+        <location filename="../../window/widgets/managewidget.cpp" line="38"/>
+        <source>安全密钥PIN</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/managewidget.cpp" line="55"/>
+        <source>重置密钥</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/managewidget.cpp" line="57"/>
+        <source>重 置</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PasskeyModule</name>
+    <message>
+        <location filename="../../passkeymodule.cpp" line="80"/>
+        <source>Passkey</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../passkeymodule.cpp" line="126"/>
+        <location filename="../../passkeymodule.cpp" line="133"/>
+        <location filename="../../passkeymodule.cpp" line="154"/>
+        <location filename="../../passkeymodule.cpp" line="163"/>
+        <source>Passkey Manage</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../passkeymodule.h" line="42"/>
+        <source>使用物理安全密钥登录</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>PromptWidget</name>
+    <message>
+        <location filename="../../window/widgets/promptwidget.cpp" line="21"/>
+        <source>跳过</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../../module/passkeyworker.cpp" line="30"/>
+        <source>请插入安全密钥</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../module/passkeyworker.cpp" line="31"/>
+        <source>正在识别安全密钥</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../module/passkeyworker.cpp" line="32"/>
+        <source>请验证安全密钥，触摸或轻扫设备</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../module/passkeyworker.cpp" line="33"/>
+        <source>触摸超时</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../module/passkeyworker.cpp" line="33"/>
+        <location filename="../../module/passkeyworker.cpp" line="36"/>
+        <source>重试</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../module/passkeyworker.cpp" line="34"/>
+        <source>识别到未添加的安全密钥，添加后可用于登录认证</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../module/passkeyworker.cpp" line="34"/>
+        <source>添加密钥</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../module/passkeyworker.cpp" line="35"/>
+        <source>设备未认证</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../module/passkeyworker.cpp" line="35"/>
+        <source>去认证</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../module/passkeyworker.cpp" line="36"/>
+        <source>未知错误</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="15"/>
+        <location filename="../../window/widgets/managewidget.cpp" line="19"/>
+        <source>更 改</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/keypindialogctrl.cpp" line="16"/>
+        <location filename="../../window/widgets/managewidget.cpp" line="20"/>
+        <source>设 置</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/resetkeydialogctrl.cpp" line="18"/>
+        <source>重置密钥</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/resetkeydialogctrl.cpp" line="20"/>
+        <source>吊销当前系统所生成证书</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/resetkeydialogctrl.cpp" line="21"/>
+        <source>抹除所有用户存储的证书</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/resetkeydialogctrl.cpp" line="22"/>
+        <source>删除生物认证的凭据数据</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/resetkeydialogctrl.cpp" line="23"/>
+        <source>重设安全密钥的部分特性和设置</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/resetkeydialogctrl.cpp" line="25"/>
+        <source>取 消</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/resetkeydialogctrl.cpp" line="26"/>
+        <source>重 置</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/resetkeydialogctrl.cpp" line="27"/>
+        <source>完 成</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/resetkeydialogctrl.cpp" line="29"/>
+        <source>请在10秒内，触摸或轻扫设备两次</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/resetkeydialogctrl.cpp" line="30"/>
+        <source>请再次触摸或轻扫设备</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/resetkeydialogctrl.cpp" line="32"/>
+        <source>重置密钥成功!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/resetkeydialogctrl.cpp" line="33"/>
+        <source>重置密钥失败</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ResetKeyDialogCtrl</name>
+    <message>
+        <location filename="../../window/widgets/resetkeydialogctrl.cpp" line="140"/>
+        <source>重置安全密钥包含但不限以下处理，您确定要重置密钥嘛？</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/resetkeydialogctrl.cpp" line="219"/>
+        <source>请重新插入安全密钥</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/resetkeydialogctrl.cpp" line="273"/>
+        <source>正在识别安全密钥</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../../window/widgets/resetkeydialogctrl.cpp" line="450"/>
+        <source>发生错误，重置失败！</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
