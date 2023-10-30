03b410ab736b805f74bdc87f2f8bf9cedd01240a
tanfeng <tanfeng@uniontech.com>
2023-10-30 13:13:12 +0800

    feat: 翻译支持
    翻译支持
    
    Log: 翻译支持
    Task: https://pms.uniontech.com/task-view-300609.html
    Influence: 文案
    Change-Id: Ia3e9e3eb3519c17da7340b2c641a098576c0f30e

--- src/service/err.c
+++ src/service/err.c
@@ -22,61 +22,61 @@ int dp_err_code_to_json(int code, char **json)
     }
 
     if (code == FIDO_OK) {
-        strncpy(apiResult.msg, gettext("成功"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Success"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_INVALID_COMMAND) {
-        strncpy(apiResult.msg, gettext("非法命令"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Invalid command"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_INVALID_PARAMETER) {
-        strncpy(apiResult.msg, gettext("非法参数"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Invalid parameter"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_INVALID_LENGTH) {
-        strncpy(apiResult.msg, gettext("非法长度"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Invalid length"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_TIMEOUT) {
-        strncpy(apiResult.msg, gettext("超时"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Timeout"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_CHANNEL_BUSY) {
-        strncpy(apiResult.msg, gettext("设备通道忙"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Channel busy"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_LOCK_REQUIRED) {
-        strncpy(apiResult.msg, gettext("请求锁"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Lock required"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_INVALID_CHANNEL) {
-        strncpy(apiResult.msg, gettext("非法设备通道"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Invalid channel"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_UNSUPPORTED_EXTENSION) {
-        strncpy(apiResult.msg, gettext("不支持的拓展"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Unsupported"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_INVALID_CREDENTIAL) {
-        strncpy(apiResult.msg, gettext("证书错误"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Invalid credential"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_USER_ACTION_PENDING) {
-        strncpy(apiResult.msg, gettext("请验证安全密钥，触摸或轻扫设备"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Touch or swipe the security key"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_UNSUPPORTED_ALGORITHM) {
-        strncpy(apiResult.msg, gettext("不支持的算法"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Unsupported algorithm"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_OPERATION_DENIED) {
-        strncpy(apiResult.msg, gettext("操作拒绝"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Operation denied"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_UNSUPPORTED_OPTION) {
-        strncpy(apiResult.msg, gettext("不支持的操作"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Unsupported option"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_INVALID_OPTION) {
-        strncpy(apiResult.msg, gettext("非法操作"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Invalid option"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_NO_CREDENTIALS) {
-        strncpy(apiResult.msg, gettext("证书不存在"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("No credentials"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_USER_ACTION_TIMEOUT) {
-        strncpy(apiResult.msg, gettext("验证安全密钥超时"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Validation timed out"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_PIN_INVALID) {
-        strncpy(apiResult.msg, gettext("PIN错误"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Pin invalid"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_PIN_AUTH_INVALID) {
-        strncpy(apiResult.msg, gettext("PIN校验失败"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Pin auth invalid"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_PIN_NOT_SET) {
-        strncpy(apiResult.msg, gettext("PIN未设置"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Pin not set"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_PIN_REQUIRED) {
-        strncpy(apiResult.msg, gettext("请求PIN"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Pin required"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_TX) {
-        strncpy(apiResult.msg, gettext("设备传输错误"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("tx error"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_RX) {
-        strncpy(apiResult.msg, gettext("设备传输错误"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("rx error"), DP_API_RESULT_MSG_LEN);
     } else if (code == FIDO_ERR_USER_PRESENCE_REQUIRED) {
-        strncpy(apiResult.msg, gettext("请求在场证明"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("User persence required"), DP_API_RESULT_MSG_LEN);
     } else if (code == DEEPIN_ERR_DEVICE_CLAIMED) {
-        strncpy(apiResult.msg, gettext("设备被占用"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Device claimed"), DP_API_RESULT_MSG_LEN);
     } else if (code == DEEPIN_ERR_DEVICE_NOT_FOUND) {
-        strncpy(apiResult.msg, gettext("设备不存在"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Device not fuound"), DP_API_RESULT_MSG_LEN);
     } else if (code == DEEPIN_ERR_DEVICE_OPEN) {
-        strncpy(apiResult.msg, gettext("设备已连接"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Device open"), DP_API_RESULT_MSG_LEN);
     } else {
-        strncpy(apiResult.msg, gettext("未知错误"), DP_API_RESULT_MSG_LEN);
+        strncpy(apiResult.msg, gettext("Unknow error"), DP_API_RESULT_MSG_LEN);
     }
 
     apiResult.code = code;
